7b11a1f56b53328d0a55f487a1f8fec89778beb5
KwisatzX <kwisatz4@gmail.com>
2021-12-03 00:21:59 +0100

    Initial commit

---
+++ src/main/java/io/github/kwisatzx/springmvccompany/SpringMvcCompanyApplication.java
@@ -0,0 +1,13 @@
+package io.github.kwisatzx.springmvccompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcCompanyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/kwisatzx/springmvccompany/SpringMvcCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kwisatzx.springmvccompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcCompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
